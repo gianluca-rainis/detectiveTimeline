9b127ec32aa7d1f0a23533fc4e248b6d72674230
gianluca-rainis <gianlucarainis@gmail.com>
2026-01-06 21:02:08 +0100

    Fixed the db connections.

--- api/db_connection.php
+++ api/db_connection.php
@@ -1,10 +1,13 @@
 <?php
-    $host = "localhost";
-    $user = "root";
-    $password = ""; // CHANGE BEFORE RUN
-    $dbname = "detectiveTimeline";
+    $env = parse_ini_file(__DIR__ . '/../.env');
+    
+    $host = $env['host'];
+    $user = $env['user'];
+    $password = $env['password'];
+    $dbname = $env['dbName'];
+    $port = 12898;
 
-    $conn = new mysqli($host, $user, $password, $dbname);
+    $conn = new mysqli($host, $user, $password, $dbname, $port);
 
     if ($conn->connect_error) {
         echo null;
